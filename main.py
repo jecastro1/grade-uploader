7cbc0f11b1eb8f4b9db7fcda2882087e0e2cca60
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-10-09 13:40:43 -0300

    Move line

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from uploader import Uploader
 import logging
 
 if __name__ == '__main__':
-    token = ''  # Github API token
-    repo = ''  # Repository name
-    key_path = ''  # Path where the Google API key is stored
+    token = 'acc76e06955377e056cf2e0cf9c74e98fa7c5798'  # Github API token
+    key_path = 'google_keys.json'  # Path where the Google API key is stored
 
-    logging.basicConfig(level=logging.INFO)
+    repo = 'AC07'  # Repository name
+
+    logging.basicConfig(level=logging.DEBUG)
 
     uploader = Uploader(repo, token, key_path)
     uploader.upload_grades()
