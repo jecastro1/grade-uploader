01ec668cae68106b491cf94cb12055408feb91af
Alejandro Kaminetzky <ajkaminetzky@uc.cl>
2017-10-09 15:02:18 -0300

    Remove token, file name and repo

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from uploader import Uploader
 import logging
 
 if __name__ == '__main__':
-    token = 'acc76e06955377e056cf2e0cf9c74e98fa7c5798'  # Github API token
-    key_path = 'google_keys.json'  # Path where the Google API key is stored
+    token = ''  # Github API token
+    key_path = ''  # Path where the Google API key is stored
 
-    repo = 'AC07'  # Repository name
+    repo = ''  # Repository name
 
     logging.basicConfig(level=logging.DEBUG)
 
